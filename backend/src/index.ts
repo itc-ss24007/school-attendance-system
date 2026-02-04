453e01fe3790b58c9f2048e0b1f19d89f77a3b1a
itc-ss24007 <s24007@std.it-college.ac.jp>
2026-02-04 14:05:33 +0900

    deploy準備

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,8 @@
  */
 import app from "./app.js";
 
-const PORT =  5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
