64fd97dc7e9c43d65f1d068e5b3e133f953af3e8
itc-ss24007 <s24007@std.it-college.ac.jp>
2026-02-04 15:51:51 +0900

    backend deploy finished

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,6 @@ import app from "./app.js";
 
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`🚀 Server is running on port ${PORT}`);
 });
